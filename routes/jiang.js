d9ed610ab89b1c8b45f1999a5f4b93b394ceecdb
muzhiqiang <694529694@qq.com>
2015-08-24 22:22:11 +0800

    share

--- routes/jiang.js
+++ routes/jiang.js
@@ -351,13 +351,13 @@ router.get('/share',function(req,res,next){
 				var Sapi_Ticket = results.ticket;
 				console.log(Sapi_Ticket);
 				Time_Ticket = time;
-				var webUrl = "http://test.ima9ic.co/share";
+				var webUrl = "http://test.ima9ic.co"+req.url;
 				var ret = sign(Sapi_Ticket,webUrl);
 				res.render('share',{timestamp:ret.timestamp,nonceStr:ret.nonceStr,signature:ret.signature});
 			});
 		});	
 	}else{
-		var webUrl = "http://test.ima9ic.co/share";
+		var webUrl = "http://test.ima9ic.co"+req.url;
 		var ret = sign(Sapi_Ticket,webUrl);
 		res.render('share',{timestamp:ret.timestamp,nonceStr:ret.nonceStr,signature:ret.signature});
 	}
